1c2a0a7aae86243516404d3620ee9ec0d6c42c35
Sonia Zhang <skzhang@usc.edu>
2023-11-26 20:57:16 -0800

    test

--- walkie_talkie/ee250/final_proj/server.py
+++ walkie_talkie/ee250/final_proj/server.py
@@ -97,11 +97,11 @@ client = mqtt.Client()
 client.on_message = on_message
 client.on_connect = on_connect
 client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
-client.loop_start()
 
 
 if __name__ == '__main__':
 
+    client.loop_start()
     app.run(debug=False)
 
     while True:
