50105b696c93baf71382643e72c020ebe9543b1e
szhang004 <skzhang@usc.edu>
2023-11-27 16:41:45 -0800

    Update flask.py

--- walkie_talkie/ee250/final_proj/flask.py
+++ walkie_talkie/ee250/final_proj/flask.py
@@ -1,1 +1,16 @@
+from flask import Flask, jsonify, render_template, redirect, url_for
 
+app = Flask('final_proj')
+
+@app.route('/')
+def index():
+    global TRANSCRIPT
+    return render_template('index.html', user_input=TRANSCRIPT)
+
+
+@app.route('/client_callback')
+
+
+if __name__ == '__main__':
+    client.loop_start()
+    app.run(debug=False)
