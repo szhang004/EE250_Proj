e0bdbc49633ffe651f83189e428dd0a5fcb102f9
szhang004 <skzhang@usc.edu>
2023-11-27 16:59:40 -0800

    Update client.py

--- walkie_talkie/ee250/final_proj/client.py
+++ walkie_talkie/ee250/final_proj/client.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, redirect, url_for, request
+# from flask import Flask, jsonify, render_template, redirect, url_for, request
 
 import paho.mqtt.client as mqtt
 import time
@@ -28,36 +28,6 @@ SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 
-app = Flask('final_proj')
-
-TRANSCRIPT = ''
-
-def process_audio(analog_data, filename="output.wav", sample_rate=50000):
-    with wave.open(filename, "wb") as wav_file:
-        nchannels = 1
-        sampwidth = 1  # 1 byte for 8 bit
-        framerate = sample_rate
-        nframes = len(analog_data)
-        comptype = "NONE"
-        compname = "not compressed"
-
-        # Set parameters
-        wav_file.setparams((nchannels, sampwidth, framerate, nframes, comptype, compname))
-        
-        # Write raw data
-        wav_file.writeframes(analog_data)
-        
-    audio_segment = AudioSegment.from_wav("output.wav")
-
-    # Export to an MP3 file
-    audio_segment.export("output.mp3", format="mp3")
-
-    audio_file = open("output.mp3", "rb")
-
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    return transcript['text']
-
-
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
@@ -69,14 +39,10 @@ def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 def server_callback(client, userdata, msg):
-    global TRANSCRIPT
-    TRANSCRIPT = msg.payload.decode()
-    setText_norefresh(TRANSCRIPT)
+    # global TRANSCRIPT
+    transcript = msg.payload.decode()
+    setText_norefresh(transcript)
 
-@app.route('/')
-def index():
-    global TRANSCRIPT
-    return render_template('index.html', user_input=TRANSCRIPT)
 
 
 if __name__ == '__main__':
@@ -86,7 +52,6 @@ if __name__ == '__main__':
     client.on_connect = on_connect
     client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
     client.loop_start()
-    app.run(debug=False)
 
 
     speak_on = False
@@ -119,10 +84,6 @@ if __name__ == '__main__':
                 msg = ''.join([chr(x) for x in mic_readings])
                 
                 client.publish("wt/client", msg)
-                global TRANSCRIPT
-                TRANSCRIPT = process_audio(byte_string)
-
-                # print(TRANSCRIPT)
                 print("Message over")
                 setText_norefresh("Message over")
             
