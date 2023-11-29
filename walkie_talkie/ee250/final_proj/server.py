45374510d40cf01593eed41ac1e1f26d950d4cae
szhang004 <skzhang@usc.edu>
2023-11-28 16:35:14 -0800

    Update server.py

--- walkie_talkie/ee250/final_proj/server.py
+++ walkie_talkie/ee250/final_proj/server.py
@@ -12,8 +12,7 @@ app = Flask('final_proj')
 openai.api_key= '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'
 # import grovepi
 
-transcript = ''
-message_lock = threading.Lock()
+TRANSCRIPT = ''
 
 def process_audio(analog_data, filename="output.wav", sample_rate=50000):
     with wave.open(filename, "wb") as wav_file:
@@ -30,70 +29,46 @@ def process_audio(analog_data, filename="output.wav", sample_rate=50000):
         # Write raw data
         wav_file.writeframes(analog_data)
         
-        audio_segment = AudioSegment.from_wav("output.wav")
+    audio_segment = AudioSegment.from_wav("output.wav")
 
-        # Export to an MP3 file
-        audio_segment.export("output.mp3", format="mp3")
+    # Export to an MP3 file
+    audio_segment.export("output.mp3", format="mp3")
 
-        audio_file = open("output.mp3", "rb")
+    audio_file = open("output.mp3", "rb")
 
-        words = openai.Audio.transcribe("whisper-1", audio_file)
-        return words['text']
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcript['text']
         
-
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
-    client.subscribe("wt/client1", 1)
-    client.message_callback_add("wt/client1", client1_callback)
-
-    client.subscribe("wt/client2", 1)
-    client.message_callback_add("wt/client2", client2_callback)
-
-#Default message callback. Please use custom callbacks.
-def on_message(client, userdata, msg):
-    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+    client.subscribe("wt/client", 1)
+    client.message_callback_add("wt/client", client_callback)
 
 
-def client1_callback(client, userdata, msg):
-    byte_string = msg.payload
-
-    # audio_bytes = bytearray(byte_string)
-    global transcript
-    transcript = process_audio(byte_string)
-        
-    client.publish("wt/server", transcript)
-    
-    print(transcript)
-    publish()
-
-
-def client2_callback(client, userdata, msg):
-    byte_string = msg.payload
-
-    # audio_bytes = bytearray(byte_string)
-    global transcript
-    transcript = process_audio(byte_string)
-
-    client.publish("wt/server", transcript)
-
-    print(transcript)
-    publish()
-    
-    
 @app.route('/')
 def index():
-    return render_template('index.html', user_input=transcript)
+    global TRANSCRIPT
+    return render_template('index.html', user_input=TRANSCRIPT)
 
 
-@app.route('/publish/message')
-def publish():
-    # return render_template('display.html', user_input=transcript)
-    return redirect(url_for ('index'))
+@app.route('/client_callback')
+def client_callback(client, userdata, msg):
+    
+    byte_string = msg.payload
+    
+    global TRANSCRIPT
+    TRANSCRIPT = process_audio(byte_string)
 
+    client.publish("wt/server", TRANSCRIPT)
+
+    print(TRANSCRIPT)
+
+    # return redirect(url_for('index'))
+    
+ 
 client = mqtt.Client()
-client.on_message = on_message
 client.on_connect = on_connect
 client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
 
