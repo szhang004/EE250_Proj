9a50d68af714c54480f6d4400516cf0df31265f1
szhang004 <skzhang@usc.edu>
2023-11-28 16:30:28 -0800

    Update server.py

--- walkie_talkie/ee250/final_proj/server.py
+++ walkie_talkie/ee250/final_proj/server.py
@@ -6,6 +6,7 @@ import openai
 from pydub import AudioSegment
 import io
 import wave
+import threading
 
 app = Flask('final_proj')
 
@@ -29,49 +30,64 @@ def process_audio(analog_data, filename="output.wav", sample_rate=50000):
         # Write raw data
         wav_file.writeframes(analog_data)
         
-    audio_segment = AudioSegment.from_wav("output.wav")
+        audio_segment = AudioSegment.from_wav("output.wav")
 
-    # Export to an MP3 file
-    audio_segment.export("output.mp3", format="mp3")
+        # Export to an MP3 file
+        audio_segment.export("output.mp3", format="mp3")
 
-    audio_file = open("output.mp3", "rb")
+        audio_file = open("output.mp3", "rb")
 
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    return transcript['text']
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        return transcript['text']
         
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
-    client.subscribe("wt/client", 1)
-    client.message_callback_add("wt/client", client_callback)
+    client.subscribe("wt/client1", 1)
+    client.message_callback_add("wt/client1", client1_callback)
+
+    client.subscribe("wt/client2", 1)
+    client.message_callback_add("wt/client2", client2_callback)
+
+#Default message callback. Please use custom callbacks.
+def on_message(client, userdata, msg):
+    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 
+@app.route('/client_callback')
+def client_callback(client, userdata, msg):
+    byte_string = msg.payload
+
+    # audio_bytes = bytearray(byte_string)
+    global TRANSCRIPT
+    TRANSCRIPT = process_audio(byte_string)
+
+    client.publish("wt/server", TRANSCRIPT)
+
+    print(transcript)
+
+    # return redirect(url_for('index'))
+    
+    
 @app.route('/')
 def index():
     global TRANSCRIPT
     return render_template('index.html', user_input=TRANSCRIPT)
 
 
-@app.route('/client_callback')
-def client_callback(client, userdata, msg):
-    
-    byte_string = msg.payload
-    
-    global TRANSCRIPT
-    TRANSCRIPT = process_audio(byte_string)
 
-    client.publish("wt/server", TRANSCRIPT)
+client = mqtt.Client()
+client.on_message = on_message
+client.on_connect = on_connect
+client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
 
-    print(TRANSCRIPT)
 
-    # return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
 
-    client.loop_start()
+    mqtt_thread = threading.Thread(target=client.loop_forever)
+    mqtt_thread.start()
     app.run(debug=False)
