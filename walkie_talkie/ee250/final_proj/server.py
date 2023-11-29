9509702b902ce6f8127190c23be1e5325154a580
szhang004 <skzhang@usc.edu>
2023-11-28 16:25:35 -0800

    Update server.py

--- walkie_talkie/ee250/final_proj/server.py
+++ walkie_talkie/ee250/final_proj/server.py
@@ -1,4 +1,4 @@
-# from flask import Flask, jsonify, render_template, redirect, url_for
+from flask import Flask, jsonify, render_template, redirect, url_for
 
 import paho.mqtt.client as mqtt
 import time
